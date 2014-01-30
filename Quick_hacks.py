efc6777b65f11d3068e556eb48bac218082f053c
Ekta Grover <ekta1007@gmail.com>
2014-01-30 10:59:19 +0530

    Update Quick_hacks.py

--- Quick_hacks.py
+++ Quick_hacks.py
@@ -4,3 +4,10 @@
 # Initializing a list of alphabets 
 from string import ascii_letters
 ascii_letters
+
+#Alternative methods to initize an array of chars
+map(chr, range(97, 123))
+# ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+map(chr, range(65, 91))
+# ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
